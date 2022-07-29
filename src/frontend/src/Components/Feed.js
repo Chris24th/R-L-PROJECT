452fc62fe0eaa2f53ba0a4188b70f26b6db9d14b
Christopher Espenida <espenida.c@sprobe.com>
2022-07-29 14:10:03 +0800

    .

--- src/frontend/src/Components/Feed.js
+++ src/frontend/src/Components/Feed.js
@@ -6,12 +6,8 @@ import Logo from "../PostelloLogo.png";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-<<<<<<< HEAD
 import axios from "axios";
-=======
-import Comment from "./Comment.js";
 import Edit_DeleteModal from "./Edit_DeleteModal.js";
->>>>>>> 2c9f55b726bfd5b0734b71bf69dedf9f81cbdb20
 
 const Feed = ({ postDetails }) => {
     const [loading, setLoading] = useState(true);
